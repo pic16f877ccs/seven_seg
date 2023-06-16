9c0ffb9c794c85245090649dcf2f8f414bddeaea
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-06-16 09:30:08 +0200

    Add four digit with decimal point

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,9 @@
 //! );
 //! ```
 use cattocol::{by_four_lines, by_lines, by_three_lines};
+use num::Bounded;
 
+#[rustfmt::skip]
 const SEVEN_SEG: [&str; 11] = [
     "┏━━━┓\n┃   ┃\n┃   ┃\n┃   ┃\n┗━━━┛\n",
     "    ╻\n    ┃\n    ┃\n    ┃\n    ╹\n",
@@ -38,6 +40,54 @@ const SEVEN_SEG: [&str; 11] = [
     "┏━━━┓\n┃   ┃\n┗━━━┫\n    ┃\n╺━━━┛\n",
     "     \n     \n╺━━━╸\n     \n     \n",
 ];
+
+#[rustfmt::skip]
+const SEVEN_SEG_DP: [&str; 40] = [
+    "┏━━━┓\n┃   ┃\n┃   ┃\n┃   ┃\n┗━━━┛\n",
+    "    ╻\n    ┃\n    ┃\n    ┃\n    ╹\n",
+    "╺━━━┓\n    ┃\n┏━━━┛\n┃    \n┗━━━╸\n",
+    "╺━━━┓\n    ┃\n╺━━━┫\n    ┃\n╺━━━┛\n",
+    "╻   ╻\n┃   ┃\n┗━━━┫\n    ┃\n    ╹\n",
+    "┏━━━╸\n┃    \n┗━━━┓\n    ┃\n╺━━━┛\n",
+    "┏━━━╸\n┃    \n┣━━━┓\n┃   ┃\n┗━━━┛\n",
+    "╺━━━┓\n    ┃\n    ┃\n    ┃\n    ╹\n",
+    "┏━━━┓\n┃   ┃\n┣━━━┫\n┃   ┃\n┗━━━┛\n",
+    "┏━━━┓\n┃   ┃\n┗━━━┫\n    ┃\n╺━━━┛\n",
+
+    "┏━━━┓ \n┃   ┃ \n┃   ┃ \n┃   ┃ \n┗━━━┛⦁\n",
+    "    ╻ \n    ┃ \n    ┃ \n    ┃ \n    ╹⦁\n",
+    "╺━━━┓ \n    ┃ \n┏━━━┛ \n┃     \n┗━━━╸⦁\n",
+    "╺━━━┓ \n    ┃ \n╺━━━┫ \n    ┃ \n╺━━━┛⦁\n",
+    "╻   ╻ \n┃   ┃ \n┗━━━┫ \n    ┃ \n    ╹⦁\n",
+    "┏━━━╸ \n┃     \n┗━━━┓ \n    ┃ \n╺━━━┛⦁\n",
+    "┏━━━╸ \n┃     \n┣━━━┓ \n┃   ┃ \n┗━━━┛⦁\n",
+    "╺━━━┓ \n    ┃ \n    ┃ \n    ┃ \n    ╹⦁\n",
+    "┏━━━┓ \n┃   ┃ \n┣━━━┫ \n┃   ┃ \n┗━━━┛⦁\n",
+    "┏━━━┓ \n┃   ┃ \n┗━━━┫ \n    ┃ \n╺━━━┛⦁\n",
+
+    "  ┏━━━┓\n  ┃   ┃\n━━┃   ┃\n  ┃   ┃\n  ┗━━━┛\n",
+    "      ╻\n      ┃\n    ━━┃\n      ┃\n      ╹\n",
+    "  ╺━━━┓\n      ┃\n━━┏━━━┛\n  ┃    \n  ┗━━━╸\n",
+    "  ╺━━━┓\n      ┃\n━━╺━━━┫\n      ┃\n  ╺━━━┛\n",
+    "  ╻   ╻\n  ┃   ┃\n━━┗━━━┫\n      ┃\n      ╹\n",
+    "  ┏━━━╸\n  ┃    \n━━┗━━━┓\n      ┃\n  ╺━━━┛\n",
+    "  ┏━━━╸\n  ┃    \n━━┣━━━┓\n  ┃   ┃\n  ┗━━━┛\n",
+    "  ╺━━━┓\n      ┃\n    ━━┃\n      ┃\n      ╹\n",
+    "  ┏━━━┓\n  ┃   ┃\n━━┣━━━┫\n  ┃   ┃\n  ┗━━━┛\n",
+    "  ┏━━━┓\n  ┃   ┃\n━━┗━━━┫\n      ┃\n  ╺━━━┛\n",
+
+    "  ┏━━━┓ \n  ┃   ┃ \n━━┃   ┃ \n  ┃   ┃ \n  ┗━━━┛⦁\n",
+    "      ╻ \n      ┃ \n    ━━┃ \n      ┃ \n      ╹⦁\n",
+    "  ╺━━━┓ \n      ┃ \n━━┏━━━┛ \n  ┃     \n  ┗━━━╸⦁\n",
+    "  ╺━━━┓ \n      ┃ \n━━╺━━━┫ \n      ┃ \n  ╺━━━┛⦁\n",
+    "  ╻   ╻ \n  ┃   ┃ \n━━┗━━━┫ \n      ┃ \n      ╹⦁\n",
+    "  ┏━━━╸ \n  ┃     \n━━┗━━━┓ \n      ┃ \n  ╺━━━┛⦁\n",
+    "  ┏━━━╸ \n  ┃     \n━━┣━━━┓ \n  ┃   ┃ \n  ┗━━━┛⦁\n",
+    "  ╺━━━┓ \n      ┃ \n    ━━┃ \n      ┃ \n      ╹⦁\n",
+    "  ┏━━━┓ \n  ┃   ┃ \n━━┣━━━┫ \n  ┃   ┃ \n  ┗━━━┛⦁\n",
+    "  ┏━━━┓ \n  ┃   ┃ \n━━┗━━━┫ \n      ┃ \n  ╺━━━┛⦁\n",
+];
+
 
 /// One digits seven segment digital display.
 ///
@@ -495,3 +545,68 @@ pub fn sevseg_four_iter(string: &str) -> Option<impl Iterator<Item = &str>> {
     ))
 }
 
+/// Four digit seven segment digital display with decimal point.
+///
+/// # Examples
+///
+/// ```
+/// # use seven_seg::sevseg_four_dp;
+/// let four_digits = sevseg_four_dp(8023_u16);
+///
+/// assert_eq!(&four_digits, "┏━━━┓ ┏━━━┓ ╺━━━┓ ╺━━━┓ \n\
+///                           ┃   ┃ ┃   ┃     ┃     ┃ \n\
+///                           ┣━━━┫ ┃   ┃ ┏━━━┛ ╺━━━┫ \n\
+///                           ┃   ┃ ┃   ┃ ┃         ┃ \n\
+///                           ┗━━━┛ ┗━━━┛ ┗━━━╸ ╺━━━┛⦁\n"
+/// );
+/// ```
+///
+/// ```
+/// # use seven_seg::sevseg_four_dp;
+/// let four_digits = sevseg_four_dp(80.23_f32);
+///
+/// assert_eq!(&four_digits, "┏━━━┓ ┏━━━┓  ╺━━━┓ ╺━━━┓\n\
+///                           ┃   ┃ ┃   ┃      ┃     ┃\n\
+///                           ┣━━━┫ ┃   ┃  ┏━━━┛ ╺━━━┫\n\
+///                           ┃   ┃ ┃   ┃  ┃         ┃\n\
+///                           ┗━━━┛ ┗━━━┛⦁ ┗━━━╸ ╺━━━┛\n"
+/// );
+/// ```
+///
+/// ```
+/// # use seven_seg::sevseg_four_dp;
+/// let four_digits = sevseg_four_dp(80234_u32);
+///
+/// assert_eq!(&four_digits, "┏━━━┓ ┏━━━┓ ╺━━━┓ ╺━━━┓\n\
+///                           ┃   ┃ ┃   ┃     ┃     ┃\n\
+///                           ┣━━━┫ ┃   ┃ ┏━━━┛ ╺━━━┫\n\
+///                           ┃   ┃ ┃   ┃ ┃         ┃\n\
+///                           ┗━━━┛ ┗━━━┛ ┗━━━╸ ╺━━━┛\n"
+/// );
+/// ```
+pub fn sevseg_four_dp<T>(n: T) -> String
+where
+    T: std::fmt::Display + Bounded,
+{
+    let string = n.to_string();
+    let mut arr = [if &string[..1] == "-" { 20 } else { 0 }, 0, 0, 0];
+    let iter = string.chars();
+    let mut len = iter.clone().count();
+    let dp_pos = if let Some(pos) = string.find('.') { pos - 1 } else { len -= 1; usize::MAX };
+    #[allow(clippy::if_same_then_else)]
+    for (i, digit) in iter.enumerate()
+        .filter_map(|idx_chr| idx_chr.1.to_digit(10)
+            .map(|digit| 
+                if idx_chr.0 == dp_pos {
+                    10
+                } else if idx_chr.0 == len {
+                    10
+                } else { 0 } + digit as usize
+            )
+        ).enumerate().take(4){
+
+        arr[i] += digit;
+    }
+
+    by_four_lines( SEVEN_SEG_DP[arr[0]], SEVEN_SEG_DP[arr[1]], SEVEN_SEG_DP[arr[2]], SEVEN_SEG_DP[arr[3]]).collect::<String>()
+}
